ac3a7346cbb0728b92fe86b737d8a75eb3124825
Sagar <sagar_karathiya@yahoo.com>
2018-03-21 15:08:21 +0530

    mongo db url change

--- src/server/config/mongo.js
+++ src/server/config/mongo.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/test');
-// mongoose.connect('mongodb://mayur:mayur@ds019956.mlab.com:19956/smart-interviewer-new');
+//mongoose.connect('mongodb://localhost:27017/test');
+mongoose.connect('mongodb://sagar:sagar@ds121099.mlab.com:21099/heroku_pkhz3bmd');
 
 module.exports = mongoose;
