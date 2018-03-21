7256e193a1bc04751fcb648f4902961eec896f83
Sagar <sagar_karathiya@yahoo.com>
2018-03-21 16:11:26 +0530

    mongo url

--- src/server/config/mongo.js
+++ src/server/config/mongo.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 
 //mongoose.connect('mongodb://localhost:27017/test');
-mongoose.connect('mongodb://sagar:sagar@ds121099.mlab.com:21099/heroku_pkhz3bmd');
+//mongoose.connect('mongodb://sagar:sagar@ds121099.mlab.com:21099/heroku_pkhz3bmd');
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost:27017/test');
 
 module.exports = mongoose;
